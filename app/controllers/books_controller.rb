bd3fc5db5b4e4e5251bda1bb02337fe2e6ee0e72
EC2 Default User <ec2-user@ip-172-31-47-108.ap-northeast-1.compute.internal>
2023-04-16 12:59:11 +0000

    [Finish]show,index,deleteの画面と機能__残りedit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,34 @@
 class BooksController < ApplicationController
   def new
   end
-
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path(book.id)
+  end
+    
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to book
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
